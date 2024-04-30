532a8500de009bc0e4d358f07a98b4790b9ccbd2
Matiascod5 <127553238+Matiascod5@users.noreply.github.com>
2024-04-29 23:01:06 -0400

    Ultimo Commit

--- Tarea1.py
+++ Tarea1.py
@@ -1,6 +1,6 @@
 import threading as th
 import time as t 
-import random as r
+
 
 
 def leerArchivo():
@@ -18,7 +18,7 @@ print(sillasE, "",barberos,"",sillasB)
 print(clientes)
 
 
-#sem_entra_cliente = th.Semaphore(num_clientes)
+detener_evento = th.Event()
 sem_sillasE = th.Semaphore(sillasE)
 sem_Barberos = th.Semaphore(barberos)
 sem_sillasB = th.Semaphore(sillasB)
@@ -38,64 +38,73 @@ def Cortarpelo(num_cliente):
     global g_sillasB
     global g_barberos
 
-
     tiempo_entrada, tiempo_espera, tiempo_corte = clientes[num_cliente]
 
-    #sem_clienteEspera = th.Semaphore(tiempo_espera)
-    
-
     #Entra un cliente
-    
     sem_mostrar.acquire()
     print(f"entra cliente {num_cliente} a barberia\n")
     sem_mostrar.release()
+    #t.sleep(tiempo_entrada)
 
-    t.sleep(1)
     
     #Se sienta un cliente
     sem_sillasE.acquire()
-    g_sillasE = g_sillasE - 1
-    numSilla_espera = sillasE - g_sillasE
+    g_sillasE = g_sillasE - 1 
+    numSilla_espera = sillasE - g_sillasE  
+
+    if numSilla_espera == 0:
+        sem_mostrar.acquire()
+        print(f"cliente {num_cliente} se va de la barberia (No hay sillas de espera)")
+        sem_mostrar.release()
+        detener_evento.set()
+        
     sem_mostrar.acquire()
     print(f"cliente {num_cliente} usa silla de espera {numSilla_espera}\n")
     sem_mostrar.release()
-    
+    sem_sillasE.release()
+
+    """acum =+ t.sleep(tiempo_espera)
+    if tiempo_espera2 > acum:
+        print(f"cliente {num_cliente} se va de la barberia (Excede su timepo limite)")
+        detener_evento.set()"""
+
     #Se sienta un cliente en una silla de barbero
-    
     sem_sillasB.acquire()
     g_sillasB = g_sillasB - 1
     g_sillasE = g_sillasE + 1 #Aumenta el numero de sillas de espera
     numSilla_barbero = sillasB - g_sillasB
+
+    """if numSilla_barbero < sillasB:
+        sem_mostrar.acquire()
+        print(f"cliente {num_cliente} usa silla de espera {numSilla_espera}\n")
+        sem_mostrar.release()"""
+    
     sem_mostrar.acquire()
     print(f"cliente {num_cliente} usa silla de barbero {numSilla_barbero}\n")
     sem_mostrar.release()
     sem_sillasB.release()
-    t.sleep(tiempo_corte)
 
     #Barbero corta el pelo 
     sem_listoParaCorte.release()
-    
-
     sem_Barberos.acquire()
     g_barberos= g_barberos- 1
     num_barbero = barberos-g_barberos
-    t.sleep(tiempo_corte)
+
+    
     sem_mostrar.acquire()
     print(f"barbero {num_barbero} atiende a cliente {num_cliente}\n")
     sem_mostrar.release()
     sem_Barberos.release()
+    t.sleep(tiempo_corte)
 
     #Cliente Listo
     sem_clienteAtendido.acquire()
-    g_barberos = g_barberos + 1 #Aumenta la cantidad de sillas de barberos
-    g_barberos = g_barberos + 1 #Aumenta la cantidad de barberos
+    g_sillasB =+ 1 #Aumenta la cantidad de sillas de barberos
+    g_barberos =+ 1 #Aumenta la cantidad de barberos
     sem_mostrar.acquire()
     print(f"sale cliente {num_cliente} (atendido por completo)\n")
     sem_mostrar.release()
     t.sleep(1)
-
-
-    #t.sleep(r.randrange(3))
 
 
 def Barbero():
@@ -104,8 +113,6 @@ def Barbero():
 
     while(True): #Entrada de clientes
         sem_listoParaCorte.acquire()
-        #sem_clienteEsperaCorte
-
         sem_Barberos.acquire()
         sem_Barberos.release()
 
@@ -120,13 +127,8 @@ th_barberos = []
 
 for i in range(barberos):
     th_barb = th.Thread(target=Barbero, daemon = True)
-    #th_barb.setDaemon(True)
     th_barberos.append(th_barb)
     th_barb.start()
-
-
-
-
 
 
 th_clientes = []
@@ -136,10 +138,8 @@ for j in range(num_clientes):
     tiempo_entrada, tiempo_espera, tiempo_corte = clientes[j]
     t.sleep(tiempo_entrada)
     th_cliente = th.Thread(target = Cortarpelo, daemon = True, args = [j])
-    #th_cliente.setDaemon(True)
     th_clientes.append(th_cliente)
     th_cliente.start()
-
 
 
 for k in th_clientes:
